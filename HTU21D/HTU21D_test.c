382010361159675df3e5352726d059600d8814be
Hank Barta <hbarta@gmail.com>
2017-11-15 16:17:37 +0000

    initial commit for HTU21D code

---
+++ HTU21D/HTU21D_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include "wiringPi.h"
+#include "wiringPiI2C.h"
+
+#include "HTU21D.h"
+
+int main ()
+{
+	wiringPiSetup();
+	int fd = wiringPiI2CSetup(HTU21D_I2C_ADDR);
+	if ( 0 > fd )
+	{
+		fprintf (stderr, "Unable to open I2C device: %s\n", strerror (errno));
+		exit (-1);
+	}
+	
+	printf("%5.2fF\n", getTemperature(fd)/5.0*9.0+32);
+	printf("%5.2f%%rh\n", getHumidity(fd));
+	
+	return 0;
+}
---
+++ HTU21D/README.md
@@ -0,0 +1,4 @@
+# Acknowledgements 
+The original code in this directory (slightly modified prior to the first
+commit came from https://github.com/leon-anavi/rpi-examples and is licensed 
+under MIT.
