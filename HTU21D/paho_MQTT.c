7f28e4487e0aad5f41840eb7e3648419526946b3
Hank Barta <hbarta@gmail.com>
2017-11-16 11:21:26 -0600

    cleanup and suppress otput

--- HTU21D/paho_MQTT.c
+++ HTU21D/paho_MQTT.c
@@ -1,21 +1,13 @@
-#include "stdio.h"
-#include "stdlib.h"
 #include "string.h"
 #include "MQTTClient.h"
 #include "paho_MQTT.h"
 
-// gcc -Wall -o paho_MQTT -lpaho-mqtt3c paho_MQTT.c
-
-//#define ADDRESS     "tcp://oak:1883"
 #define CLIENTID    "ExampleClientPub"
-//#define TOPIC       "home_automation/niwot/familyroom/msg"
-//#define PAYLOAD     "Hello World!"
 #define QOS         0
 #define TIMEOUT     10000L
 
 static MQTTClient client;
 static MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-//static MQTTClient_message pubmsg = MQTTClient_message_initializer;
 static MQTTClient_deliveryToken token;
 
 /** @file routines to publish to an MQTT server
@@ -29,11 +21,10 @@ static MQTTClient_deliveryToken token;
  * @param address is of the form "//tcp://<host>:port" (e.g "tcp://oak:1883")
  * @param client_id to use for the session
  * @retval 0 on success or one of the errors listed in MQTTClient.h or
- *  -1 tro indicate that a previous session exists.
+ *  -1 to indicate that a previous session exists.
  */
 int init_MQTT(const char* address, const char* client_id)
 {
-    //conn_opts = MQTTClient_connectOptions_initializer;
     int rc;
 
     if(client != 0)
@@ -49,12 +40,22 @@ int init_MQTT(const char* address, const char* client_id)
 
     if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
     {
-        printf("Failed to connect, return code %d\n", rc);
         return rc;
     }
     return 0;
 }
 
+/** @brief
+ * Publish to an MQTT server (after calling init_MQT())
+ * @param topic
+ * @param payload
+ * @retval 0 on success or one of the errors listed in MQTTClient.h or
+ *  -1 to indicate that a previous session exists.
+ * 
+ * @TODO check result of MQTTClient_publishMessage()
+ * @TODO determine if MQTTClient_waitForCompletion() is needed 
+ * when QOS == 0
+ */
 int publish_MQTT(const char* topic, const char* payload)
 {
     int rc;
@@ -65,11 +66,7 @@ int publish_MQTT(const char* topic, const char* payload)
     pubmsg.qos = QOS;
     pubmsg.retained = 0;
     MQTTClient_publishMessage(client, topic, &pubmsg, &token);
-    printf("Waiting for up to %d seconds for publication of %s\n"
-            "on topic %s for client with ClientID: %s\n",
-            (int)(TIMEOUT/1000), payload, topic, CLIENTID);
     rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
-    printf("Message with delivery token %d delivered\n", token);
     return rc;
 }
 
