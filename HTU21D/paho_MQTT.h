3c385351e8447216c3637f7a9b1f200d2138258e
Hank Barta <hbarta@gmail.com>
2017-11-16 00:48:53 +0000

    declarations for routiones in paho_MQTT.c

---
+++ HTU21D/paho_MQTT.h
@@ -0,0 +1,9 @@
+#ifndef __PAHO_MQTT_H__
+#define __PAHO_MQTT_H__
+
+// user code will beed to #include "MQTTClient.h"
+int init_MQTT(const char* address, const char* client_id);
+
+
+
+#endif	//  __PAHO_MQTT_H__
