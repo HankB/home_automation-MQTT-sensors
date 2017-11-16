b0f735a44b69af37e50fd49a964aea5e8530929a
Hank Barta <hbarta@gmail.com>
2017-11-16 08:38:44 -0600

    functionality split into callable routines

--- HTU21D/paho_MQTT.c
+++ HTU21D/paho_MQTT.c
@@ -6,16 +6,16 @@
 
 // gcc -Wall -o paho_MQTT -lpaho-mqtt3c paho_MQTT.c
 
-#define ADDRESS     "tcp://oak:1883"
+//#define ADDRESS     "tcp://oak:1883"
 #define CLIENTID    "ExampleClientPub"
 //#define TOPIC       "home_automation/niwot/familyroom/msg"
 //#define PAYLOAD     "Hello World!"
-//#define QOS         1
+#define QOS         0
 #define TIMEOUT     10000L
 
 static MQTTClient client;
-static MQTTClient_connectOptions conn_opts;
-static MQTTClient_message pubmsg;
+static MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+//static MQTTClient_message pubmsg = MQTTClient_message_initializer;
 static MQTTClient_deliveryToken token;
 
 /** @file routines to publish to an MQTT server
@@ -33,8 +33,7 @@ static MQTTClient_deliveryToken token;
  */
 int init_MQTT(const char* address, const char* client_id)
 {
-    conn_opts = MQTTClient_connectOptions_initializer;
-    pubmsg = MQTTClient_message_initializer;
+    //conn_opts = MQTTClient_connectOptions_initializer;
     int rc;
 
     if(client != 0)
@@ -43,7 +42,7 @@ int init_MQTT(const char* address, const char* client_id)
         return MQTTCLIENT_FAILURE;
     }
 
-    MQTTClient_create(&client, ADDRESS, CLIENTID,
+    MQTTClient_create(&client, address, client_id,
         MQTTCLIENT_PERSISTENCE_NONE, NULL);
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 1;
@@ -56,38 +55,27 @@ int init_MQTT(const char* address, const char* client_id)
     return 0;
 }
 
-int main(int argc, char* argv[])
+int publish_MQTT(const char* topic, const char* payload)
 {
-    /*(
-    MQTTClient client;
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    MQTTClient_deliveryToken token;
     int rc;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
 
-    MQTTClient_create(&client, ADDRESS, CLIENTID,
-        MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    conn_opts.keepAliveInterval = 20;
-    conn_opts.cleansession = 1;
-
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
-    {
-        printf("Failed to connect, return code %d\n", rc);
-        exit(-1);
-    }
-    */
-
-    pubmsg.payload = PAYLOAD;
-    pubmsg.payloadlen = strlen(PAYLOAD);
+    pubmsg.payload = (char*) payload;
+    pubmsg.payloadlen = strlen(payload);
     pubmsg.qos = QOS;
     pubmsg.retained = 0;
-    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+    MQTTClient_publishMessage(client, topic, &pubmsg, &token);
     printf("Waiting for up to %d seconds for publication of %s\n"
             "on topic %s for client with ClientID: %s\n",
-            (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
+            (int)(TIMEOUT/1000), payload, topic, CLIENTID);
     rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
     printf("Message with delivery token %d delivered\n", token);
+    return rc;
+}
+
+void cleanup_MQTT(void)
+{
     MQTTClient_disconnect(client, 10000);
     MQTTClient_destroy(&client);
-    return rc;
+    client = 0;
 }
