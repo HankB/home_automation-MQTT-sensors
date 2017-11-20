a684eb4423f9ff01e073b1a4afd7c29156c541c0
Hank Barta <hbarta@gmail.com>
2017-11-19 22:52:17 -0600

    added code to publish to MQTT

--- HTU21D/HTU21D_publish.c
+++ HTU21D/HTU21D_publish.c
@@ -3,10 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include "wiringPi.h"
 #include "wiringPiI2C.h"
-
+#include "MQTTClient.h"
+#include "paho_MQTT.h"
+#
 #include "HTU21D.h"
+
+#define BUFLEN  1024
+static char     topic_buf[BUFLEN];
+static char     payload_buf[BUFLEN];
+static char     host_buf[BUFLEN];
+
+#define ADDR        "tcp://oak:1883"
+#define CLIENT_ID   "ExampleClientPub"
+
 
 void usage(const char* prog)
 {
@@ -21,6 +33,12 @@ int main (int argc, char** argv)
     const char *	description = 0;
     const char * 	location = 0;
 
+    if(gethostname(host_buf, BUFLEN))
+    {
+        perror("Error: ");
+        strncpy(host_buf, "somehost", BUFLEN);
+    }
+ 
     // parse command line args
     while ((opt = getopt(argc, argv, "i:l:d:")) != -1)
     {
@@ -70,9 +88,17 @@ int main (int argc, char** argv)
 	usage(argv[0]);
     }
 
-    printf("interval:%d, location:%s, description:%s\n", interval, location, description);
+    snprintf(topic_buf, BUFLEN, "home_automation/%s/%s/%s", host_buf,
+	    location, description);
+    printf("interval:%d, location:%s, description:%s\n%s\n", interval,
+	    location, description, topic_buf);
 
-    return 0;
+    int     rc;
+
+    rc = init_MQTT(ADDR, CLIENT_ID);
+    printf("init_MQTT():%d\n", rc);
+
+    //return 0;
 
     int fd = wiringPiI2CSetup(HTU21D_I2C_ADDR);
     if ( 0 > fd )
@@ -81,8 +107,18 @@ int main (int argc, char** argv)
 	exit (-1);
     }
 
-    printf("%5.2fF\n", getTemperature(fd)/5.0*9.0+32);
-    printf("%5.2f%%rh\n", getHumidity(fd));
+    while(1)	// add logic for handling broken MQTT server connection
+    {
+	float	temperature = getTemperature(fd)/5.0*9.0+32;
+	float	humididy = getHumidity(fd);
 
+	printf("%5.2fF\n", temperature);
+	printf("%5.2f%%rh\n", humididy);
+
+	snprintf(payload_buf, BUFLEN, "%d, %5.2f, %5.2f", time(0), temperature, humididy);
+	publish_MQTT(topic_buf, payload_buf);
+	printf("publish_MQTT():%s\n", payload_buf);
+	sleep(15*interval);
+    }
     return 0;
 }
