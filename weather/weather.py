da4fc70e52e9132817037100d351679a8d82fc37
Hank Barta <hbarta@gmail.com>
2017-12-11 21:30:12 -0600

    looping to collect weather

--- weather/weather.py
+++ weather/weather.py
@@ -45,11 +45,16 @@ parser.add_argument('--apikey', '-k', metavar="<API key>",
                     nargs=1, required=True, help='broker host name')
 parser.add_argument('--test', '-t', # metavar="<test option>",
                     action='store_true', help='use canned response')
+parser.add_argument("-v", "--verbosity", 
+                    help="increase output verbosity",
+                    action="store_true")
 args = parser.parse_args()
-print("zip code", args.zip[0])
-print("MQTT broker", args.broker[0])
-print("API key", args.apikey[0])
-print("test option", args.test)
+verbose = args.verbosity
+if verbose:
+    print("zip code", args.zip[0])
+    print("MQTT broker", args.broker[0])
+    print("API key", args.apikey[0])
+    print("test option", args.test)
 
 """
 Functions related to publishing MQTT messages
@@ -99,43 +104,46 @@ def delay_to_interval(minutes=15):
     delay_sec = minutes*60 - int(time.time())%(minutes*60)
     time.sleep(delay_sec)
 
+update_interval = 5		# minutes
+delay_to_interval(update_interval)
+
 # some day loop here. For now just pass through once
-timestamp = int(time.time())
+while True:    
+    timestamp = int(time.time())
 
-# temporary test code - use canned data to avoid repeated calls to the server
-if args.test == True:
-    print("use test data")
-    r='{"coord":{"lon":-88.15,"lat":41.87},"weather":[{"id":804,"main":   "Clouds","description":"overcast clouds","icon":"04n"}],"base":"stations",    "main":{"temp":272.05,"pressure":1018,"humidity":68,"temp_min":271.15,"temp_max":273.15},"visibility":16093,"wind":{"speed":2.6,"deg":340},"clouds":{"all":90},"dt":1512953100,"sys":{"type":1,"id":1007,"message":0.1736,"country":"US","sunrise":1512997823,"sunset":1513030899},"id":0,"name":"Winfield","cod":200}'
+    # temporary test code - use canned data to avoid repeated calls to the server
+    if args.test == True:
+        print("use test data")
+        r='{"coord":{"lon":-88.15,"lat":41.87},"weather":[{"id":804,"main":   "Clouds","description":"overcast clouds","icon":"04n"}],"base":"stations",    "main":{"temp":272.05,"pressure":1018,"humidity":68,"temp_min":271.15,"temp_max":273.15},"visibility":16093,"wind":{"speed":2.6,"deg":340},"clouds":{"all":90},"dt":1512953100,"sys":{"type":1,"id":1007,"message":0.1736,"country":"US","sunrise":1512997823,"sunset":1513030899},"id":0,"name":"Winfield","cod":200}'
 
-else:
-    print("Use real server response")
-    url='http://api.openweathermap.org/data/2.5/weather?zip='+args.zip[0]+'&apikey='+args.apikey[0]
-    print("URL:", url)
-    req = urllib.request.Request(url)
-    print("req:", req)
-    r_bytes = urllib.request.urlopen(req).read()
-    r = r_bytes.decode('UTF-8')
+    else:
+        print("Use real server response")
+        url='http://api.openweathermap.org/data/2.5/weather?zip='+args.zip[0]+'&apikey='+args.apikey[0]
+        print("URL:", url)
+        req = urllib.request.Request(url)
+        print("req:", req)
+        r_bytes = urllib.request.urlopen(req).read()
+        r = r_bytes.decode('UTF-8')
 
 
-##parsing response
-#cont = json.loads(r.decode('utf-8'))
-cont = json.loads(r)
-print("cont:", cont)
+    ##parsing response
+    cont = json.loads(r)
+    print("cont:", cont)
 
-counter = 0
-print("\nr:", r)
-print("cont[name]:", cont['name'])
+    counter = 0
+    print("\nr:", r)
+    print("cont[name]:", cont['name'])
 
-temp_F = Kelvin_to_Fahrenheit(cont['main']['temp'])
-print("\ncont[main][temp]:", temp_F)
-print("cont[main][humidity]:", cont['main']['humidity'])
-print("cont[wind][speed]:", cont['wind']['speed'])
-print("cont[wind][deg]:", cont['wind']['deg'])
-print("update time", cont['dt'])
+    temp_F = Kelvin_to_Fahrenheit(cont['main']['temp'])
+    print("\ncont[main][temp]:", temp_F)
+    print("cont[main][humidity]:", cont['main']['humidity'])
+    print("cont[wind][speed]:", cont['wind']['speed'])
+    print("cont[wind][deg]:", cont['wind']['deg'])
+    print("update time", cont['dt'])
 
-print(datetime.fromtimestamp(int(cont['dt']), tz).isoformat())
-print(datetime.fromtimestamp(timestamp, tz).isoformat())
+    print(datetime.fromtimestamp(int(cont['dt']), tz).isoformat())
+    print(datetime.fromtimestamp(timestamp, tz).isoformat())
 
-publish_weather(timestamp, temp_F, cont['main']['humidity'], cont['wind']['speed'], cont['wind']['deg'])
+    publish_weather(timestamp, temp_F, cont['main']['humidity'], cont['wind']['speed'], cont['wind']['deg'])
 
-time.sleep(1)
+    delay_to_interval(update_interval)
