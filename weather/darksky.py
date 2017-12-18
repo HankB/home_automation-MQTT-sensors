e6c430a45456f8eb0420fc8d4b5aae532270700b
Hank Barta <hbarta@gmail.com>
2017-12-18 15:39:50 -0600

    scripts to collect/publish weather fro Dark Sky

---
+++ weather/darksky.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+'''
+Script to fetch current weather from darksky.net, parse it and submit to
+the MQTT broker on oak.
+Command line arguments
+-l --location <coordinates>
+-k --apikey API kep provided by accuweather
+-t --test simulate with test response
+-v --verbosity increase verbosity of console output
+
+first two aguments required
+
+Location code found at "City Search" at
+https://developer.accuweather.com/accuweather-locations-api/apis
+
+This script produces the payload on stdout which can be piped to 
+`mosquitto_pub` to publish
+'''
+import argparse
+import urllib.request
+import json
+import sys
+import time
+
+'''
+Print verbose output to stderr. The only thing that goes to stdout
+is the MQTT payload
+'''
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+'''
+Application logic
+First parse command line arguments
+'''
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--location', '-l', metavar="<coordinates>",
+                    nargs=1, required=True, help='specify coordinates')
+parser.add_argument('--apikey', '-k', metavar="<API key>",
+                    nargs=1, required=True, help='provide API key')
+parser.add_argument('--test', '-t', # metavar="<test option>",
+                    action='store_true', help='use canned response')
+parser.add_argument("--verbosity", "-v", 
+                    help="increase output verbosity",
+                    action="store_true")
+args = parser.parse_args()
+verbose = args.verbosity
+
+if verbose:
+    eprint("coordinates", args.location[0])
+    eprint("API key", args.apikey[0])
+    eprint("test option", args.test)
+
+timestamp = int(time.time())
+
+# test code - use canned data to avoid repeated calls to the server. We get 50 calls/day on the free tier
+if args.test == True:
+    if verbose: eprint("use test data")
+    r='{"latitude":41.866611,"longitude":-88.160051,"timezone":"America/Chicago","currently":{"time":1513630000,"summary":"Partly Cloudy","icon":"partly-cloudy-day","nearestStormDistance":19,"nearestStormBearing":215,"precipIntensity":0,"precipProbability":0,"temperature":44.28,"apparentTemperature":40.6,"dewPoint":36.68,"humidity":0.74,"pressure":1012.25,"windSpeed":6.44,"windGust":13.92,"windBearing":241,"cloudCover":0.37,"uvIndex":1,"visibility":8.19,"ozone":324.26},"offset":-6}'
+
+else:
+    if verbose: eprint("Use real server response")
+    url='https://api.darksky.net/forecast/'+args.apikey[0]+'/'+args.location[0]+'?exclude=minutely,hourly,daily,alerts,flags'
+    if verbose: eprint("URL:", url)
+    req = urllib.request.Request(url)
+    r_bytes = urllib.request.urlopen(req).read()
+    r = r_bytes.decode('UTF-8')
+
+# parse response
+cont = json.loads(r)
+if verbose: eprint("cont:", cont)
+
+if verbose: eprint("\nr\n:", r)
+
+if verbose: eprint("cont[currently][time]:", cont['currently']['time'])
+if verbose: eprint("cont[currently][temperature]:", cont['currently']['temperature'])
+if verbose: eprint("cont[currently][humidity]:", cont['currently']['humidity'])
+if verbose: eprint("cont[currently][windBearing]:", cont['currently']['windBearing'])
+if verbose: eprint("cont[currently][windSpeed]:", cont['currently']['windSpeed'])
+if verbose: eprint("cont[currently][summary]:", cont['currently']['summary'])
+
+# write values of interestweather to stdout
+print(  timestamp, 
+        cont['currently']['time'], 
+        cont['currently']['temperature'], 
+        cont['currently']['humidity'], 
+        cont['currently']['windBearing'], 
+        cont['currently']['windSpeed'],
+        cont['currently']['summary'])
+sys.exit(0)
---
+++ weather/darksky.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# command line arguments:  API key [host]
+
+if [ $# -le 1 ]
+then
+    echo "Usage: $0 API_key location [broker_host]"
+    echo "location: lattitude,longitude e.g.\"41.875574,-87.6465083\""
+    exit 1
+fi
+
+key=$1
+location=$2
+
+shift; shift
+
+if [ $# -eq 0 ]
+then
+    host=localhost
+else
+    host=$1
+fi
+
+# ID our host
+HOSTNAME=`hostname`
+
+# add user's !/bin to PATH
+PATH=${HOME}/bin:$PATH
+
+echo darksky.py -l $location -k $key -v
+
+echo darksky.py -l $location -k $key -v 2>>/tmp/darksky.txt | \
+mosquitto_pub -s -t home_automation/$HOSTNAME/darksky/weather -h $host
