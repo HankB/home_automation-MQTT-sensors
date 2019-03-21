2fa188695499235c09b8af276199ee274d1e8e8d
Hank Barta <hbarta@gmail.com>
2019-03-21 10:17:50 -0500

    strip out all paho-mqtt stuff

--- DS18B20/ds18b20-temp.py
+++ DS18B20/ds18b20-temp.py
@@ -8,42 +8,9 @@ for original code
 import glob
 import time
 
-### copied from https://www.eclipse.org/paho/clients/python/
-import paho.mqtt.client as mqtt
-
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    # client.subscribe("$SYS/#")
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-
-def on_publish(client, userdata, mid):
-    print("on_publish(", client, userdata, mid, ")")
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.on_publish = on_publish
-
-client.connect("oak", 1883, 60)	# connect to my MQTT server
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface. (changed to non-blocking call)
-client.loop_start()
-### end of included code
-
-
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
-update_interval = 5		# minutes
 
 
 def read_temp_raw():
@@ -64,28 +31,9 @@ def read_temp():
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c, temp_f
 	
-""" 
-Delay to the next minute interval some integral number of intervals
-from time zero.
-"""
-def delay_to_interval(minutes=15):
-    delay_sec = minutes*60 - int(time.time())%(minutes*60)
-    time.sleep(delay_sec)
-
-topic = "home_automation/niwot/basement/freezer_temp"
+# topic = "home_automation/niwot/basement/freezer_temp"
+timeStamp = int(time.time())
 
 def publish_temperature(timestamp, temperature):
     payload = "{0:12.0F}, {1:3.1F}".format(timestamp,temperature)
-    print("publishing", payload)
-    client.publish(topic,payload, qos=0, retain=True)    
-
-delay_to_interval(update_interval)
-
-print("starting main loop")
-
-while True:
-    timeStamp = int(time.time())
-    temp = read_temp()[1]
-    print(timeStamp, temp)
-    publish_temperature(timeStamp, temp)
-    delay_to_interval(update_interval)
+    print("payload)
