66d4dfa1d4739615809586a72f2cf5d4c9941649
Hank Barta <hbarta@gmail.com>
2019-03-22 19:57:56 -0500

    delete newline and extra spaces, 2nd try

--- DS18B20/publish-freezer-temp.sh
+++ DS18B20/publish-freezer-temp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # meant to be run fron cron
 # */5 * * * * /home/pi/bin/publish-freezer-temp.sh
-/home/pi/bin/ds18b20-temp.py |tr -d " \n"| /usr/bin/mosquitto_pub -s -t "home_automation/niwot/basement/freezer_temp" -h oak
+/home/pi/bin/ds18b20-temp.py | sed -e "s/  //"|tr -d '\n'| /usr/bin/mosquitto_pub -s -t "home_automation/niwot/basement/freezer_temp" -h oak
